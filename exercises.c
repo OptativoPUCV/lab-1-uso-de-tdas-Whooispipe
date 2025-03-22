ad71ff2472c3fb1f8a849b260e122555908dab11
Your Name <you@example.com>
2025-03-22 19:59:55 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -131,7 +131,7 @@ int parentesisBalanceados(char *cadena)
    {
       if(cadena[i] == ')' || cadena[i] == ']' || cadena[i] == '}')
       {
-         pushBack(pilalaotramitad , cadena[i]);
+         pushBack(*pilalaotramitad , cadena[i]);
       }
    }
    
