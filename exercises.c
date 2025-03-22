ab7b3d0d82342a2e1a4c39b9983d63086a0b73b5
Your Name <you@example.com>
2025-03-22 19:15:56 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -130,7 +130,7 @@ int parentesisBalanceados(char *cadena)
       }
       else if(cadena[i] == ')')
       {
-         if(is_empty(pila))
+         if(top(pila) == NULL)
          {
             return 0;
          }
